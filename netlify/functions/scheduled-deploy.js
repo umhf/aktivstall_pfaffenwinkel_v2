c4ad22f556916e5a6df8d87903603eb922dfee4a
umhf <uli@maruli.org>
2022-11-14 08:37:05 +0100

    add scheduled deploy test

---
+++ netlify/functions/scheduled-deploy.js
@@ -0,0 +1,15 @@
+import fetch from "node-fetch"
+import { schedule } from '@netlify/functions'
+const BUILD_HOOK = "https://api.netlify.com/build_hooks/6371e615528c7870cd4659e3";
+
+export const handler = schedule("40,45,50 0-23 * * *", async () => {
+    await fetch(BUILD_HOOK, {
+        method: "POST"
+    }).then(response => {
+        console.log("Build hook response:", response)
+    })
+    
+    return {
+        statusCode: 200
+    }
+})
