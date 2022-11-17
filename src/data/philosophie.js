9e28164589bcd3155cb8bcfab8154fc26ad29994
umhf <uli@maruli.org>
2022-11-17 17:30:38 +0100

    blabla

---
+++ src/data/philosophie.js
@@ -0,0 +1,24 @@
+export const philosophien = [
+    {
+      headline: "Aktivstall mit Paddocktrail",
+      text: `Ein Aktivstall steht für größtmögliche Bewegungsfreiheit, natürliche gemeinsame Großherdenhaltung von Stuten und Wallachen sowie ein auf unterschiedlichste Bedürfnisse maßgeschneidertes automatisiertes Fütterungsmanagement.
+
+      Die wissenschaftlichen Erkenntnisse der letzten Jahre haben eindeutig festgeschrieben, dass die Gesunderhaltung unserer Pferde von drei maßgeblichen Säulen/Faktoren unterstützt wird.`,
+    },
+    {
+      headline: "Bewegungsfreiheit",
+      text: `... an 365 Tagen im Jahr und dies selbstverständlich unter den Voraussetzungen permanent zugänglicher Rückzugs- und Ruheorte. Ausreichend regen- und sonnengeschützten Unterständen sowie unendlichen Laufwege und Paddocktrails die mit geeigneten Bewegungsanreizen bestückt sind. Unser Aktivstall bietet dies auf über einem Hektar`,
+    },
+    {
+      headline: "Großherdenhaltung",
+      text: `... in alters- und geschlechtergemischten Herden entsprechen in jeder Hinsicht dem natürlichen Verhalten von Wildpferden. Erst in Großherden können sich die dringend erforderlichen Untergruppen bilden die es rangniedrigen sowie aktiveren Pferden erst ermöglicht, sich Gleichgesinnten anzuschließen ohne ausgegrenzt zu sein. Es gibt somit keine Verlierer in einer Herde sondern ausgeglichene „Interessengemeinschaften“ die zudem jederzeit ihrem eigenen Rhythmus folgen können. Unser Aktivstall bietet das`,
+      ctaText: "Kontakt",
+      link: "/kontakt"
+    },
+    {
+      headline: "Fütterungsmanagement",
+      text: `... durch ein vollelektronisches Fütterungsmanagement ist es möglich, jedem einzelnen Pferd der Herde gerecht zu werden. Da der größte Feind unserer Pferde die durch Überfütterung verursachten Zivilisationskrankheiten sind und diese rapide zugenommen haben liegt der Augenmerk ganz besonders auf eine rationierte, dem Grundbedarf angemessene Heufütterung, die durch entsprechend elektronische Futterraufen gesteuert wird. Alle darüber hinaus gehenden Bedürfnisse (z.b. alter Pferde) sollte durch zusätzliche Angebote abgedeckt sein. Unser Aktivstall bietet das`,
+      ctaText: "Kontakt",
+      link: "/kontakt"
+    },
+  ];
